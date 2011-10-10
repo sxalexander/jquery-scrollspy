7b9148a0368ecde50e86a5261a4cdbba256ac10b
Sam Alexander <sxalexander@gmail.com>
2011-10-09 22:44:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# jquery-scrollspy
+
+An adaptation of the Mootools Scrollspy ( http://davidwalsh.name/mootools-scrollspy ) plugin for jQuery
+
+(c) 2011 Samuel Alexander (samuel@madewithothers.com)
+
+Released under The MIT License.
+
+## Description:
+
+scrollspy is a simple jQuery plugin for firing events based on where the user has scrolled to in a page.
+
+
+## Homepage:
+
+http://github.com/sxalexander/jquery-scrollspy
+
+## Source:
+
+Hosted at GitHub; browse at:
+
+  http://github.com/sxalexander/jquery-scrollspy/tree/master
+
+Or clone from:
+
+    git://github.com/sxalexander/jquery-scrollspy.git
+
+## Usage:
+
+1. Insert the necessary elements in your document's `<head>` section, e.g.:
+   
+        <script type='text/javascript' src='/javascripts/jquery.scrollspy.js'></script>
+
+ Remember to include jquery.scrollspy.js *after* including the main jQuery library.
+
+2. Initialise scrollspy in your document.onload, e.g.:
+
+        <script type='text/javascript'>
+         $(function() {
+	       $('.topbar').scrollspy({
+	           min: 20, 
+	           max: window.height(), 
+	           onEnter: function(element, position){
+	               $(element).css('position', 'fixed');
+	           },
+	           onLeave: function(element, position){
+	               $(element).css('position', 'relative')
+	           }
+	           });
+	     });
+        </script>
+
+Check out the /examples for more info !
+
+## Documentation:
+
+Options for ScrollSpy include:
+    min: (defaults to 0) The minimum value of the X or Y coordinate, depending on mode.
+    max: (defaults to 0) The maximum value of the X or Y coordinate, depending on mode.
+    mode: (defaults to 'vertical') Defines whether to listen to X or Y scrolling.
+    container: (defaults to window) The element whose scrolling to listen to.
+
+Events for ScrollSpy include:
+    scrollTick: Fires on each scroll event within the min and max parameters. Receives as parameters:
+        position: an object with the current X and Y position.
+        inside: a Boolean value for whether or not the user is within the min and max parameters
+        enters: the number of times the min / max has been entered.
+        leaves: the number of times the min / max has been left.
+    scrollEnter: Fires every time the user enters the min / max zone.
+        position: an object with the current X and Y position.
+        enters: the number of times the min / max has been entered.
+    scrollLeave: Fires every time the user leaves the min / max zone.
+        position: an object with the current X and Y position.
+        leaves: the number of times the min / max has been left.
+
+
+## A note on forking:
+
+By forking this project you hereby grant permission for any commits to your fork to be
+merged back into this repository and, with attribution, be released under the terms of
+the MIT License.
---
+++ jquery-scrollspy.js
@@ -0,0 +1,100 @@
+/*!
+ * jQuery Scrollspy Plugin
+ * Author: @sxalexander
+ * Licensed under the MIT license
+ */
+
+
+;(function ( $, window, document, undefined ) {
+
+    $.fn.extend({
+      scrollspy: function ( options ) {
+        
+          var defaults = {
+            min: 0,
+            max: 0,
+            mode: 'vertical',
+            buffer: 50,
+            container: window,
+            onEnter: options.onEnter ? options.onEnter : [],
+            onLeave: options.onLeave ? options.onLeave : [],
+            onTick: options.onTick ? options.onTick : []
+          }
+          
+          // Here's a 'best' approach for overriding 'defaults' with specified options. 
+          // Note how rather than a regular defaults object being passed as the second
+          // parameter, we instead refer to $.fn.pluginName.options explicitly, merging it
+          // with the options passed directly to the plugin. This allows us to override
+          // options both globally and on a per-call level. 
+
+          var options = $.extend( {}, defaults, options );
+
+          var init = function(){
+            
+          }
+          
+          var processScroll = function(){
+
+            
+          }
+          
+          return this.each(function (i) {
+
+              var element = this;
+              var o = options;
+              var $container = $(o.container);
+              var mode = o.mode;
+              var buffer = o.buffer;
+              var enters = leaves = 0;
+              var max = parseInt(o.max);
+              var inside = false;
+              
+              /* fix max */
+              if(max == 0){
+                max = (mode == 'vertical') ? $container.height() : $container.width();
+              }
+              
+              /* add listener to container */
+              $container.bind('scroll', function(e){
+                  var position = {top: $(this).scrollTop(), left: $(this).scrollLeft()};
+                  var xy = (mode == 'vertical') ? position.top + buffer : position.left + buffer;
+                  /* if we have reached the minimum bound but are below the max ... */
+                  if(xy >= o.min && xy <= max){
+                    /* trigger enter event */
+                    if(!inside){
+                       inside = true;
+                       enters++;
+                       
+                       /* fire enter event */
+                       $(element).trigger('scrollEnter', {position: position})
+                       if($.isFunction(o.onEnter)){
+                         o.onEnter(element, position);
+                       }
+                      
+                     }
+                     
+                     /* triger tick event */
+                     $(element).trigger('scrollTick', {position: position, inside: inside, enters: enters, leaves: leaves})
+                     if($.isFunction(o.onTick)){
+                       o.onTick(element, position, inside, enters, leaves);
+                     }
+                  }else{
+                    
+                    if(inside){
+                      inside = false;
+                      leaves++;
+                      $(element).trigger('scrollLeave', {position: position, leaves:leaves})
+                      if($.isFunction(o.onLeave)){
+                        o.onLeave(element, position);
+                      }
+                    }
+                  }
+              }); 
+
+          });
+      }
+
+    })
+
+    
+})( jQuery, window );
